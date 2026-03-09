fefd06224b4db4d93abbb9bddecab8a07a2d9a65
uy633733-sys <uy633733@gmail.com>
2026-03-10 02:09:30 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Navbar Active Link Switch
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", function () {
+
+        navLinks.forEach(l => l.classList.remove("active"));
+
+        this.classList.add("active");
+    });
+});
